5aeccf297e5ee5cd1576dd3be972555dec196d8e
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-05-19 16:25:15 -0500

    middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,5 +56,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'Duncan' => \App\Http\Middleware\Duncan::class,
     ];
 }
