3237931743b8a1c93b67e5209fa4eac43d7817c4
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-06-06 14:52:22 -0500

    contact margin-bottom

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container-fluid" style="padding: 0">
 @include('_includes.colorDesk')
-  <div class="col-md-12" style="background-color: #000; margin-bottom: 2em">
+  <div class="col-md-12" style="background-color: #000; margin-bottom: 5em">
   
       <center><h1 class="os-animation" data-os-animation="foo fadeIn" data-os-animation-delay="0s">Contact Me</h1></center>
       <div class="col-md-6 col-md-offset-3">
