9dda281b65f643ed664fbf1244de3bb41cb37781
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-06-07 16:39:50 -0500

    nots

--- resources/views/_includes/navbar.blade.php
+++ resources/views/_includes/navbar.blade.php
@@ -4,6 +4,7 @@
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" style="background-color: #000" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
+            <div class="hamburgerNotsTop"><div class="lilNots">{{App\Contact::newCons()}}</div></div>
             <span class="icon-bar" style="background-color: #fff"></span>
             <span class="icon-bar" style="background-color: #fff"></span>
             <span class="icon-bar" style="background-color: #fff"></span>
@@ -22,14 +23,13 @@
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                     {{ Auth::user()->name }} <span class="caret"></span>
-                                        @if(App\Contact::newCons()!=0)
+                   @if(App\Contact::newCons()!=0)
                     <div class="lilNotsTop"><div class="lilNots">{{App\Contact::newCons()}}</div></div>
                     @endif
                   </a>
                     <ul class="dropdown-menu" role="menu">
-                                          <li><a href="{{url('/contact')}}">Inbox</a></li>
+                      <li><a href="{{url('/contact')}}">Inbox</a></li>
                       <li><a href="{{url('/landing/1/edit')}}">Edit Home Page</a></li>
-
                       {{-- <li><a href="{{url('/project/create')}}">Add Project</a></li> --}}
                       <li>
                         <a href="{{ url('/logout') }}"
